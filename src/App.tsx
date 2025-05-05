a868c839057b53cc785fc7b50760ba788f918ded
aysenur-aydn <aysenurbyrby@gmail.com>
2025-05-05 11:52:37 +0300

    change password

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LoginFormik } from './Components/Login/LoginForm';
+import { Provider } from "./components/ui/provider";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { createContext } from 'react';
+import Test from './Components/Test';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  return (
+    <Provider>
+        <Router>
+          <Routes>
+          <Route path="/" Component={LoginFormik}/>
+          <Route path="test" Component={Test}/>
+          </Routes>
+        </Router>
+      </Provider>
+        )
+}
+export default App
